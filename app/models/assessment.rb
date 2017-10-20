f298e944f50265c3ee91af30192490d8b593229d
MBKeeler <mark.keeler@comcast.net>
2017-10-20 11:31:46 -0400

    Adds foreign keys and model macros
    -- Adds foreign keys to user and player referencing assessment
    -- Adds belong_to and has_many macros to these Models: User, Player,
    Assessment.

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Assessment < ApplicationRecord
+  belongs_to :player, inverse_of: :assessments
+  belongs_to :user, inverse_of: :assessments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020143615) do
+ActiveRecord::Schema.define(version: 20171020152208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20171020143615) do
     t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "assessment_id"
+    t.index ["assessment_id"], name: "index_players_on_assessment_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,9 +52,13 @@ ActiveRecord::Schema.define(version: 20171020143615) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "assessment_id"
+    t.index ["assessment_id"], name: "index_users_on_assessment_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "players", "assessments"
+  add_foreign_key "users", "assessments"
 end
