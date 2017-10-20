26e1b03c06322c8ea5930202be89dde0e04fc847
MBKeeler <mark.keeler@comcast.net>
2017-10-20 10:30:02 -0400

    Adds two basic curl scripts to create & destroy
    Adds two basic curl scripts to create and destroy a player.

---
+++ scripts/players/create.sh
@@ -0,0 +1,14 @@
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/players"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "example": {
+      "text": "'"${TEXT}"'"
+    }
+  }'
+
+echo
