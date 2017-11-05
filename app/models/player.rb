67cfdaf0359238c765b265625e5d65e79a2c1df9
MBKeeler <mark.keeler@comcast.net>
2017-11-05 14:23:37 -0500

    Added validation to player model
    Added additional validation to player model to ensure input is an
    integer and that the maximum value is 5 for scores.  Also, checks that
    the length of the score fields is 1.  Age is allowed to have a length of
    two.

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,15 +5,15 @@ class Player < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :age, presence: true, length: { maximum: 2 }
+  validates :age, presence: true, length: { maximum: 2 }, numericality: { only_integer: true, less_than: 14 }
   validates :grade, presence: true, length: { maximum: 1 }
   validates :program, length: { maximum: 250 }
   validates :notes, length: { maximum: 250 }
-  validates :catch, presence: true, length: { maximum: 1 }
-  validates :throw, presence: true, length: { maximum: 1 }
-  validates :pitch, presence: true, length: { maximum: 1 }
-  validates :bat, presence: true, length: { maximum: 1 }
-  validates :athleticism, presence: true, length: { maximum: 1 }
+  validates :catch, presence: true, length: { maximum: 1 }, numericality: { only_integer: true, less_than: 6 }
+  validates :throw, presence: true, length: { maximum: 1 }, numericality: { only_integer: true, less_than: 6 }
+  validates :pitch, presence: true, length: { maximum: 1 }, numericality: { only_integer: true, less_than: 6 }
+  validates :bat, presence: true, length: { maximum: 1 }, numericality: { only_integer: true, less_than: 6 }
+  validates :athleticism, presence: true, length: { maximum: 1 }, numericality: { only_integer: true, less_than: 6 }
   #  we do not need to validates a note. This is optional
   # we want to validate assessments when a player is made
 end
